0e9ff33bd681135880ecd6fae8592f2259056a1e
Tin Theethawat Savastham <theethawat.s@outlook.com>
2025-05-23 00:03:29 +0700

    💚 Initial Dockerfile from NextJS Official Doc

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://tdckhawtang.blob.core.windows.net/**")],
   },
+  output: "standalone",
 };
 
 export default nextConfig;
